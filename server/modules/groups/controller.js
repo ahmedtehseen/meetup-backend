0fd85caf7ef338682661efd761f210e48cdc10aa
ahmedtehseen <ahmedpisces7@gmail.com>
2017-07-25 12:58:31 +0500

    add getGroupsMeetup controller

--- server/modules/groups/controller.js
+++ server/modules/groups/controller.js
@@ -1,4 +1,5 @@
 import Group from './model';
+import { Meetup } from '../meetups';
 
 export const createGroup = async (req, res) => {
   const {
@@ -59,10 +60,29 @@ export const createGroupMeetup = async (req, res) => {
   }
 
   try {
-    const [meetup, group] =  await Group.addMeetup(groupId, { title, description });
+    const { meetup, group } =  await Group.addMeetup(groupId, { title, description });
 
     return res.status(201).json({ error: false, meetup, group });
   } catch(e) {
     return res.status(400).json({ error: true, message: 'Meetup cannot be created!' });
   }
 }
+
+
+// search for see if group exist
+export const getGroupsMeetups = async (req, res) => {
+  const { groupId } = req.params;
+
+  if (!groupId) {
+    return res.status(400).json({ error: true, message: 'you need to provide a group id' })
+  }
+
+  try {
+    return res.status(200).json({
+      error: false,
+      meetups: await Meetup.find({ group: groupId }).populate('group', 'name')
+    });
+  } catch (e) {
+    return res.status(400).json({ error: true, message: 'Did not fetch meetup'})
+  };
+};
--- server/modules/groups/model.js
+++ server/modules/groups/model.js
@@ -22,18 +22,28 @@ const GroupSchema = new Schema({
   }]
 }, { timestamps: true });
 
+// create a meetup and add it to Group
+
 GroupSchema.statics.addMeetup = async function(id, args) {
   const Meetup = mongoose.model('Meetup');
 
-  const group = await this.findById(id);
+  // const group = await this.findById(id);
+  // we add the group ID to the meetup group element
+  // finally this is the author of the meetups element
+  const meetup = await new Meetup({ ...args, group: id });
+  // we found the group with the id provided in the url
+  // And we pushed the meetup ID in the meetups element
+  const group = await this.findByIdAndUpdate(id, { $push: { meetups: meetup.id } });
 
-  const meetup = await new Meetup({ ...args, group });
+  // group.meetups.push(meetup);
 
-  group.meetups.push(meetup);
+  // const result = await Promise.all([meetup.save(), group.save()]);
 
-  const result = await Promise.all([meetup.save(), group.save()]);
-
-  return result;
+  // return result;
+  return {
+    meetup: await meetup.save(),
+    group
+  };
 };
 
 export default mongoose.model('Group', GroupSchema);
